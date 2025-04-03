5bf663b72da0a68ec8b052385f55d70a205f2308
Horse-Lips <CallMeP@outlook.com>
2025-04-03 22:41:10 +0100

    Updating project.html/projects.js
    - projects.html
    	- Updated right side content to display dynamic skills list
    - projects.js
    	- Added skills list to each project
    	- Updated loadProject function to dynamically generate skills list

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,35 +1,43 @@
 const projects = {
     "graphCompression": {
-        title: "Graph Compression",
+        title: "Approximate Graph Compression",
         description: "Graph Compression project details",
+        skills: ["Java", "Graph Theory", "Compression Algorithms"],
     },
     "localisation": {
-        title: "Localisation",
+        title: "Localisation Strategies in Trees",
         description: "Localisation project details",
+        skills: ["Python", "Graph Theory", "Localisation"],
     },
     "maximumMatching": {
-        title: "Maximum Matching",
+        title: "Approximate Maximum Matching",
         description: "Maximum matching project details",
+        skills: ["Java", "Graph Theory", "Maximum Matching"],
     },
     "covid": {
-        title: "Covid Tracker",
+        title: "Cisco Meraki Covid Tracker",
         description: "Covid tracker project details",
+        skills: ["Python", "Django", "Agile"],
     },
     "deepLearning": {
         title: "Deep Learning",
         description: "Deep Learning project details",
+        skills: ["Python", "Jupyter", "sklearn"],
     },
     "machineLearning": {
         title: "Machine Learning",
         description: "Machine Learning project details",
+        skills: ["Python", "Jupyter", "sklearn"],
     },
     "ai": {
         title: "Reinforcement Learning",
         description: "Reinforcement project details",
+        skills: ["Python", "Jupyter", "sklearn"],
     },
     "constraintProgramming": {
         title: "Constraint Programming",
         description: "Constraint Programming project details",
+        skills: ["Python", "ILP", "Constraint Programming"],
     }
 };
 
@@ -39,5 +47,16 @@ function loadProject(projectId) {
 
     document.getElementById("project-title").textContent = project.title;
     document.getElementById("project-description").textContent = project.description;
+
+    const skills = document.getElementById("skills");
+    skills.innerHTML = "";
+
+    const ul = document.createElement("ul");
+    project.skills.forEach(skill => {
+        const li = document.createElement("li");
+        li.textContent = skill;
+        ul.appendChild(li);
+    });
+    skills.appendChild(ul);
 }
 
